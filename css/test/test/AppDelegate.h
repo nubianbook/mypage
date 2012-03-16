3f1d53984181702dee6144962fcc600d39fc5b53
nubianbook <nubianbook@me.com>
2012-03-15 22:04:52 -0400

    Try again

---
+++ css/test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Carol Gaylor on 3/15/12.
+//  Copyright (c) 2012 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ css/test/testTests/testTests.h
@@ -0,0 +1,13 @@
+//
+//  testTests.h
+//  testTests
+//
+//  Created by Carol Gaylor on 3/15/12.
+//  Copyright (c) 2012 Apple Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface testTests : SenTestCase
+
+@end
